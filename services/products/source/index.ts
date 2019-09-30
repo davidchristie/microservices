87860b958f68d99160ff69fd9e08a2ad6c46c661
David Christie <davidchristie.nz@gmail.com>
2019-09-30 20:50:33 +0200

    Add unit tests for server module

--- services/products/source/index.ts
+++ services/products/source/index.ts
@@ -1,34 +1,4 @@
-import { buildFederatedSchema } from "@apollo/federation";
-import { ApolloServer, gql } from "apollo-server";
-import resolvers from "./resolvers";
-
-const typeDefs = gql`
-  extend type Mutation {
-    createProduct(name: String!): Product!
-    deleteProduct(id: String!): Product!
-  }
-
-  extend type Query {
-    getProduct(id: ID): Product!
-    getProducts: [Product]
-  }
-
-  type Product @key(fields: "id") {
-    id: String!
-    name: String!
-    price: Int
-    weight: Int
-  }
-`;
-
-const server = new ApolloServer({
-  schema: buildFederatedSchema([
-    {
-      resolvers,
-      typeDefs
-    }
-  ])
-});
+import server from "./server";
 
 server.listen({ port: process.env.PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
---
+++ services/products/source/server.ts
@@ -0,0 +1,33 @@
+import { buildFederatedSchema } from "@apollo/federation";
+import { ApolloServer, gql } from "apollo-server";
+import resolvers from "./resolvers";
+
+const typeDefs = gql`
+  extend type Mutation {
+    createProduct(name: String!): Product!
+    deleteProduct(id: String!): Product!
+  }
+
+  extend type Query {
+    getProduct(id: ID): Product!
+    getProducts: [Product]
+  }
+
+  type Product @key(fields: "id") {
+    id: String!
+    name: String!
+    price: Int
+    weight: Int
+  }
+`;
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([
+    {
+      resolvers,
+      typeDefs
+    }
+  ])
+});
+
+export default server;
