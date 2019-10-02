46810d422c75150fcd7b9796212f1434d07b3166
David Christie <davidchristie.nz@gmail.com>
2019-10-02 20:47:39 +0200

    Add development environment

--- services/accounts/source/passwords.ts
+++ services/accounts/source/passwords.ts
@@ -2,10 +2,13 @@ import bcrypt from "bcryptjs";
 
 const SALT_ROUNDS = 10;
 
-export const checkPassword = (password: string, hash: string): Promise<boolean> => {
+export const checkPassword = (
+  password: string,
+  hash: string
+): Promise<boolean> => {
   return bcrypt.compare(password, hash);
-}
+};
 
 export const hashPassword = (password: string): Promise<string> => {
   return bcrypt.hash(password, SALT_ROUNDS);
-}
+};
--- services/products/source/neo4j.ts
+++ services/products/source/neo4j.ts
@@ -3,7 +3,7 @@ import { Driver } from "neo4j-driver/types/v1";
 
 export const createDriver = (): Driver => {
   const url = process.env.NEO4J_HOST || "bolt://products-neo4j:7687";
-  const user = process.env.NEO4J_USER || "neoj4";
+  const user = process.env.NEO4J_USERNAME || "neoj4";
   const password = process.env.NEO4J_PASSWORD || "products";
   return neo4j.driver(url, neo4j.auth.basic(user, password));
 };
