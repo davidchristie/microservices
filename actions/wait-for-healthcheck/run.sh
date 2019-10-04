f2fa9c22c039dea514ee8ca4864c45aa23c75195
David Christie <davidchristie.nz@gmail.com>
2019-10-04 17:48:49 +0200

    Wait for products database

---
+++ actions/wait-for-healthcheck/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+function wait-for-healthcheck() {
+  healthcheck=$@
+  attempt_counter=0
+  max_attempts=30
+  
+  until $healthcheck; do
+
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "maximum attempts reached"
+      exit 1
+    fi
+
+    echo "..."
+    attempt_counter=$(($attempt_counter+1))
+    sleep 5
+  done
+  echo "ready"
+}
+
+wait-for-healthcheck $@
