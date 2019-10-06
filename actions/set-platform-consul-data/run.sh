6920697c44fc54cadf81984c405d4be88f501700
David Christie <davidchristie.nz@gmail.com>
2019-10-06 18:26:09 +0200

    Set consul data in a transaction

--- actions/set-platform-consul-data/run.sh
+++ actions/set-platform-consul-data/run.sh
@@ -2,27 +2,9 @@
 
 set -e
 
-curl \
-    --request PUT \
-    --data "http://accounts:4000" \
-    localhost:8500/v1/kv/gateway/services/accounts/url
+DATA=$(cat actions/set-platform-consul-data/data.json)
 
 curl \
     --request PUT \
-    --data "http://customers:4000" \
-    localhost:8500/v1/kv/gateway/services/customers/url
-
-curl \
-    --request PUT \
-    --data "http://inventory:4000" \
-    localhost:8500/v1/kv/gateway/services/inventory/url
-
-curl \
-    --request PUT \
-    --data "http://products:4000" \
-    localhost:8500/v1/kv/gateway/services/products/url
-
-curl \
-    --request PUT \
-    --data "http://reviews:4000" \
-    localhost:8500/v1/kv/gateway/services/reviews/url
+    --data "$DATA" \
+    localhost:8500/v1/txn
